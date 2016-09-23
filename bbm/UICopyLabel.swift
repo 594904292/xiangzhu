a08182d3a8d2c7d3fea5e5bdd154b490bda76470
songgc <594904292@qq.com>
2016-09-23 15:49:48 +0800

    新加

---
+++ bbm/UICopyLabel.swift
@@ -0,0 +1,57 @@
+//
+//  UICopyLabel.swift
+//  bbm
+//
+//  Created by songgc on 16/9/22.
+//  Copyright © 2016年 sprin. All rights reserved.
+//
+
+import UIKit
+
+class UICopyLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        sharedInit()
+    }
+    
+    func sharedInit() {
+        userInteractionEnabled = true
+        addGestureRecognizer(UILongPressGestureRecognizer(target: self,
+            action: "showMenu:"))
+    }
+    
+    func showMenu(sender: AnyObject?) {
+        becomeFirstResponder()
+        let menu = UIMenuController.sharedMenuController()
+        if !menu.menuVisible {
+            menu.setTargetRect(bounds, inView: self)
+            menu.setMenuVisible(true, animated: true)
+        }
+    }
+    
+    //复制
+    override func copy(sender: AnyObject?) {
+        let board = UIPasteboard.generalPasteboard()
+        board.string = text
+        let menu = UIMenuController.sharedMenuController()
+        menu.setMenuVisible(false, animated: true)
+    }
+    
+    override func canBecomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func canPerformAction(action: Selector, withSender sender: AnyObject?)
+        -> Bool {
+            if action == "copy:" {
+                return true
+            }
+            return false
+    }
+}
