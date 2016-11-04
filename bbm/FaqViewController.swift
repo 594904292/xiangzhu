7f017c74ab940225de56b1bc3634b910a39396e4
songgc <594904292@qq.com>
2016-11-04 17:54:45 +0800

    swift 3.0

---
+++ bbm/FaqViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FaqViewController.swift
+//  bbm
+//
+//  Created by songgc on 16/10/12.
+//  Copyright © 2016年 sprin. All rights reserved.
+//
+
+import UIKit
+
+class FaqViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ bbm/RFCodeCountdownBtn.swift
@@ -0,0 +1,21 @@
+//
+//  RFCodeCountdownBtn.swift
+//  bbm
+//
+//  Created by songgc on 2016/11/4.
+//  Copyright © 2016年 sprin. All rights reserved.
+//
+
+import UIKit
+
+class RFCodeCountdownBtn: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
